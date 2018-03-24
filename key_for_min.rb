c9d0a7283eae88b1b5d0d38ad5179fa1a69bcffc
Mike C <mikecronin92@gmail.com>
2018-03-24 21:16:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'benchmark'
 
 def key_for_min_value(name_hash)
   min_value = Float::INFINITY
